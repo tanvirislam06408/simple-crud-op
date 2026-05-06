6bda962ef7cdb3860af40421b912ec75d1a0d742
tanvir <mstanvirislam05@gmail.com>
2026-05-06 22:29:03 +0600

    feat: add endpoint to retrieve users from MongoDB

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database=client.db('simpleCrud').collection('users');
+
+    app.get('/users',async(req,res)=>{
+        const cursor=database.find()
+        const result=await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
